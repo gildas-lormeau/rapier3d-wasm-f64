c151b2a29cff7a79ca23eca35743255ed4742079
Gildas <gildas.lormeau@gmail.com>
2025-07-14 02:52:16 +0200

    update wasm-pack and pass RUSTFLAGS

--- builds/rapier3d-wasm-simd-f64/build_rust.sh
+++ builds/rapier3d-wasm-simd-f64/build_rust.sh
@@ -3,7 +3,7 @@
 # Cleaning rust because changing rust flags may lead to different build results.
 cargo clean
 
-npx wasm-pack build
+RUSTFLAGS='-C target-feature=+simd128' npx wasm-pack build
 sed -i.bak 's#dimforge_rapier3d_f64#@dimforge/rapier3d-f64#g' pkg/package.json
 sed -i.bak 's/"rapier_wasm3d_bg.wasm"/"*"/g' pkg/package.json
 (
